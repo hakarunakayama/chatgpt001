7f58dbb82a3372d9c3001441c86c074213c90aef
Ubuntu <ubuntu@ip-172-31-5-44.ap-northeast-1.compute.internal>
2024-05-19 02:57:05 +0000

    wsgi.pyでmatplotlibsディレクトリを環境変数設定

--- wsgi.py
+++ wsgi.py
@@ -4,4 +4,6 @@ import sys
 cur_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, cur_dir)
 
+os.environ['MPLCONFIGDIR'] = '/var/www/.config/matplotlib'
+
 from app import app as application
